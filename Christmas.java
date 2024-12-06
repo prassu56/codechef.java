2261a06a65ef9c333c5538d26c71c7035236ff17
prassu56 <prasuprasanna236@gmail.com>
2024-12-06 11:57:17 +0530

    Add files via upload

---
+++ Christmas.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Christmas
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int d=sc.nextInt();
+		if(d==25)
+		{
+		    System.out.println("CHRISTMAS");
+		}
+		else{
+		    System.out.println("ORDINARY");
+		}
+
+	}
+}
