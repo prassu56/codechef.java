b4bde64e253d085ec752d2636d012835fc6d1cac
prassu56 <prasuprasanna236@gmail.com>
2024-12-06 11:42:56 +0530

    Add files via upload

---
+++ Aiiscoming.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Aiiscoming
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a<=60){
+		    System.out.println("yes");
+		}
+		else{
+		    System.out.println("no");
+		}
+
+	}
+}
