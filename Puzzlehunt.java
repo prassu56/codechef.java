f8b3d63046ba0478e516eb65b3e72d844c872b22
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-14 20:34:47 +0530

    Add files via upload

---
+++ Puzzlehunt.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class  Puzzlehunt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int N=sc.nextInt();
+	  if(N>=6&&N<=8)
+	  {
+	      System.out.println("yes");
+	  }
+	  else{
+	      System.out.println("no");
+	      
+	  }
+
+	}
+}
