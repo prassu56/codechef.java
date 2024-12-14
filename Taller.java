7bc2351c6f9b1e8394ac484d3fd362b858ad3cce
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-14 21:14:40 +0530

    Add files via upload

---
+++ Taller.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Taller
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		if(A>B)
+		{
+		    System.out.println("A");
+		}
+       else{
+            System.out.println("B");
+       }
+	}
+}
+}
