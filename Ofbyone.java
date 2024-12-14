d9ad40446ff8c782b783ffe4f254cd04b5459354
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-14 19:02:13 +0530

    Add files via upload

---
+++ Ofbyone.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Ofbyone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int A=sc.nextInt();
+	 int B=sc.nextInt();
+	 int sum=A+B;
+	 System.out.println(sum+"1");
+
+	}
+}
