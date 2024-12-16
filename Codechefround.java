338bd789bafbfa67e46f3da4ed5ac14a1193023c
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-16 12:13:34 +0530

    Add files via upload

---
+++ Codechefround.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechefround
+{
+	public static void main (String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+	    int x=sc.nextInt();
+		if(x==4)
+		{
+		 System.out.println("YES");
+		 }
+		 else{
+		     System.out.println("NO");
+		 }
+
+	}
+}
