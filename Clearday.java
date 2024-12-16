8e030b18e685f2cd0aefb3aeb87fceb792a8fe8d
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-16 12:11:00 +0530

    Add files via upload

---
+++ Clearday.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Clearday
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int C=7-(X+Y);
+        System.out.println(C);
+	}
+}
