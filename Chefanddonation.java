464645dd4892fb8607e5af32ec38a6160eb86774
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-23 12:10:40 +0530

    Add files via upload

---
+++ Chefanddonation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Chefanddonation
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    int z=y-x;
+		        System.out.println(z);
+		    }
+		
+
+	}
+}
