572c9c742e843c9ea4dd50e743c52d28e5c29f1a
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2024-12-23 12:30:57 +0530

    Add files via upload

---
+++ Getsubscription.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Getsubscription
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    int X=sc.nextInt();
+		    if(X>30)
+		    {
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }		
+		}
+	}
+}
