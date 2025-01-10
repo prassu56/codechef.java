c5ba0596b4244e0f78ab8dcdf5a4d01aa4277e37
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-01-10 20:27:23 +0530

    Add files via upload

---
+++ Doublerent.java
@@ -0,0 +1,9 @@
+/*  2//input  4//output  */
+import java.util.*;
+class Doublerent{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        System.out.println(X*2);
+    }
+}
