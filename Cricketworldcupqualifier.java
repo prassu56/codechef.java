6efc0e0cfb591e70d153b40dcf9c31eda9622a92
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-01-10 19:47:35 +0530

    Add files via upload

---
+++ Cricketworldcupqualifier.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Cricketworldcupqualifier
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		    int X=sc.nextInt();
+		    if(X>=12)
+		    {
+		        System.out.println("yes");
+		    }
+		    else{
+		        System.out.println("no");
+		    }
+		}
+
+	}
+
