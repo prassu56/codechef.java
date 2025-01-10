f5b5e3f0773e5fe6efdcb646f3238e828a7f23f1
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-01-10 20:36:36 +0530

    Add files via upload

---
+++ Savingtaxes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Savingtaxes
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		int Z=X-Y;
+		System.out.println(Z);
+
+	}
+}
+}
