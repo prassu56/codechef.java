3720215b6606ccc342ee6d34a0ea192f07e1bc91
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-01-13 21:47:14 +0530

    Add files via upload

---
+++ Notfound404.java
@@ -0,0 +1,18 @@
+/*  200//input   FOUND//output  */
+import java.util.*;
+public class Notfound404
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int X=sc.nextInt();
+	if(X==404)
+	{
+	    System.out.println("NOT FOUND");
+	}
+	else{
+	    System.out.println("FOUND");
+	}
+
+	}
+}
