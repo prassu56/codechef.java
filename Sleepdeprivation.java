747fd665fbce51f7909152a1ddb0c2a74bf82db7
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-02-14 11:02:33 +0530

    Add files via upload

---
+++ Sleepdeprivation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Sleepdeprivation
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+	    Scanner sc=new Scanner(System.in);
+	    int T=sc.nextInt();
+	    while(T-->0)
+	    {
+	    int X=sc.nextInt();
+	    if(X<7)
+	    {
+	        System.out.println("YES");
+	    }
+	    else{
+         
+              System.out.println("NO");
+	     
+	    }
+	    }
+	}
+}
+
+	
