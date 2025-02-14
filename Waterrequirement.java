986827c0cce77fb839af2de1408560415833d26d
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-02-14 11:28:08 +0530

    Add files via upload

---
+++ Waterrequirement.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Waterrequirement
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		   int N=sc.nextInt();
+		   int M=2*N;
+		   System.out.println(M);
+		}
+	}
+}
