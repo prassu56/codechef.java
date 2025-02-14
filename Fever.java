c8fac070d6a2e812e2ca3b9042fd5a242f6a18d2
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-02-14 10:56:39 +0530

    Add files via upload

---
+++ Fever.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Fever
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int T=sc.nextInt();
+	    while(T-->0)
+	    {
+	    int X=sc.nextInt();
+	    if(X>98)
+	    {
+	        System.out.println("YES");
+	    }
+	    else{
+         
+              System.out.println("NO");
+	     
+	    }
+	    }
+	}
+}
