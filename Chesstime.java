76a261b999768cf87218db4a8a24471e4d183d56
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-02-14 10:25:34 +0530

    Add files via upload

---
+++ Chesstime.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Chesstime
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int T=sc.nextInt();
+	    while(T-->0)
+	    {
+	        int X=sc.nextInt();
+	        int N=X*3;
+	        System.out.println(N);
+	    }
+	}
+}
