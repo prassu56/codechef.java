4508ca4b1daf43b31c24ce52b916ddf6da39d476
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-02-14 10:52:35 +0530

    Add files via upload

---
+++ Airqualityindex.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Airqualityindex
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    if(X<100)
+	    {
+	        System.out.println("YES");
+	    }
+	    else{
+         
+              System.out.println("NO");
+	     
+	    }
+	}
+}
