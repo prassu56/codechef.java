678e6f592b8e80cee48fbd14598fa18bcb0201f3
Bandlapalli Prasanna Kumari <prasuprasanna236@gmail.com>
2025-02-14 09:52:51 +0530

    Add files via upload

--- Manapoints.java
+++ Manapoints.java
@@ -1,3 +1,7 @@
+/* 3
+10 30/input       3/output
+6 41              6
+50 2              0      */
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -6,15 +10,13 @@ class Manapoints
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner sc=new Scanner(System.in);
-		int T=sc.nextInt();
-		while(T-->0)
-		{
-		    int X=sc.nextInt();
-		    int Y=sc.nextInt();
-		     X=Y/X;
-		    System.out.println(X);
-		}
-
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            int Y=s.nextInt();
+            X=Y/X;
+            System.out.println(X);
+        }
 	}
 }
